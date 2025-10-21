6f11fe1d6e768bb917a8a00f6e7dfff9a2689fa2
Verry Adrian <adrianverry5@gmail.com>
2025-10-21 15:02:19 +0700

    Update users migration: make 'username' and 'password' fields nullable, and add 'google_id' field

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,12 +13,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('username');
+            $table->string('username')->nullable(); 
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable(); 
             $table->string('avatar')->nullable(); 
-            $table->string('role')->default('user'); 
+            $table->string('role')->default('user');
+            $table->string('google_id')->nullable(); 
             $table->rememberToken();
             $table->timestamps();
         });
