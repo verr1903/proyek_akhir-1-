d36db0cf3678597588c204706fab4ef7f820e6e8
Verry Adrian <adrianverry5@gmail.com>
2025-11-13 08:20:58 +0700

    refactor: clean up and organize route definitions

--- routes/web.php
+++ routes/web.php
@@ -18,43 +18,63 @@ use App\Http\Controllers\Client\ProdukClientController;
 use App\Http\Controllers\Client\ProfileClientController;
 use App\Http\Controllers\Client\RiwayatClientController;
 use App\Http\Controllers\Auth\GoogleController;
-use App\Http\Controllers\Client\OrderController;
-use App\Http\Controllers\PaymentController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [DashboardClientController::class, 'index'])
    ->name('dashboard');
-Route::get('/login', [LoginController::class, 'index'])
-   ->name('login');
-Route::post('/login', [LoginController::class, 'login'])
-   ->name('login.post');
 
-Route::get('/register', [RegisterController::class, 'index'])
-   ->name('register');
-Route::post('/register', [RegisterController::class, 'store'])
-   ->name('register.store');
-
-Route::get('/auth/google', [GoogleController::class, 'redirect'])
-   ->name('google.redirect');
-Route::get('/auth/google/callback', [GoogleController::class, 'callback'])
-   ->name('google.callback');
+Route::get('/produk', [ProdukClientController::class, 'index'])
+   ->name('produk');
 
 Route::post('/logout', [LoginController::class, 'logout'])
    ->name('logout');
-Route::get('/logoutt', [LoginController::class, 'logout'])->name('logoutt');
 
+Route::get('/logoutt', [LoginController::class, 'logout'])
+   ->name('logoutt');
+
+
+
+// Routes untuk guest (belum login)
+Route::middleware('guest')->group(function () {
+   // login routes
+   Route::get('/login', [LoginController::class, 'index'])
+      ->name('login');
+   Route::post('/login', [LoginController::class, 'login'])
+      ->name('login.post');
+
+   // register routes
+   Route::get('/register', [RegisterController::class, 'index'])
+      ->name('register');
+   Route::post('/register', [RegisterController::class, 'store'])
+      ->name('register.store');
+
+   // login with google routes
+   Route::get('/auth/google', [GoogleController::class, 'redirect'])
+      ->name('google.redirect');
+
+   // register with google routes 
+   Route::get('/auth/google/callback', [GoogleController::class, 'callback'])
+      ->name('google.callback');
+});
+
+
+
+
+// Routes yang memerlukan autentikasi user
 Route::middleware('auth')->group(function () {
+
+   // detail produk routes
    Route::get('/detail/{encryptedId}', [DetailClientController::class, 'index'])
       ->name('detail');
 
 
    // keranjang routes
+   Route::get('/keranjang', [KeranjangClientController::class, 'index'])
+      ->name('keranjang');
    Route::post('/keranjang/tambah', [KeranjangClientController::class, 'store'])
       ->name('cart.add');
    Route::post('/cart/update-quantity', [KeranjangClientController::class, 'updateQuantity'])
       ->name('cart.updateQuantity');
-   Route::get('/keranjang', [KeranjangClientController::class, 'index'])
-      ->name('keranjang');
    Route::delete('/cart/{id}', [KeranjangClientController::class, 'destroy'])
       ->name('cart.destroy');
    Route::post('/checkout', [KeranjangClientController::class, 'co'])
@@ -63,13 +83,12 @@ Route::middleware('auth')->group(function () {
       ->name('midtrans.token');
 
 
-
    // lokasi routes
    Route::get('/lokasi', [LokasiClientController::class, 'index'])
       ->name('lokasi');
    Route::post('/lokasi', [LokasiClientController::class, 'store'])
       ->name('lokasi.store');
-   Route::post('/alamat/aktifkan/{id}', [App\Http\Controllers\Client\LokasiClientController::class, 'aktifkan'])
+   Route::post('/alamat/aktifkan/{id}', [LokasiClientController::class, 'aktifkan'])
       ->name('alamat.aktifkan');
    Route::delete('/alamat/hapus/{id}', [LokasiClientController::class, 'destroy'])
       ->name('alamat.destroy');
@@ -77,19 +96,23 @@ Route::middleware('auth')->group(function () {
       ->name('alamat.update');
 
 
-
+   // profile routes
    Route::get('/profile', [ProfileClientController::class, 'index'])
       ->name('profile');
    Route::post('/profile/update', [ProfileClientController::class, 'update'])
       ->name('profile.update');
 
-   Route::get('/riwayat', [RiwayatClientController::class, 'index'])->name('riwayat');
-   Route::post('/ulasan', [RiwayatClientController::class, 'store'])->name('ulasan.store');
 
-
-   Route::get('/produk', [ProdukClientController::class, 'index'])->name('produk');
+   // riwayat routes 
+   Route::get('/riwayat', [RiwayatClientController::class, 'index'])
+      ->name('riwayat');
+   Route::post('/ulasan', [RiwayatClientController::class, 'store'])
+      ->name('ulasan.store');
 });
 
+
+
+// Routes untuk admin
 Route::prefix('admin')
    ->middleware(['auth', 'role:karyawan,owner'])
    ->group(
@@ -144,7 +167,6 @@ Route::prefix('admin')
             ->name('diskonAdmin.update');
 
 
-
          // Karyawan
          Route::get('/karyawan', [KaryawanAdminController::class, 'index'])
             ->name('karyawanAdmin');
@@ -155,9 +177,12 @@ Route::prefix('admin')
          Route::delete('/karyawan/delete/{id}', [KaryawanAdminController::class, 'destroy'])
             ->name('karyawanAdmin.destroy');
 
+
          // Laporan
          Route::get('/laporan', [LaporanAdminController::class, 'index'])
             ->name('laporanAdmin');
+
+
          // Profile
          Route::get('/profile', [ProfileAdminController::class, 'index'])
             ->name('profileAdmin');
