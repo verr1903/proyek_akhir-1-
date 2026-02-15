9c69ee54dbd69e3d0539813c7edee38171566e80
Verry Adrian <adrianverry5@gmail.com>
2026-02-16 00:36:12 +0700

    fix: call function iklanSeeder in databaseSeeder

--- database/seeders/IklanSeeder.php
+++ database/seeders/IklanSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class ProductSeeder extends Seeder
+class IklanSeeder extends Seeder
 {
     public function run(): void
     {
@@ -14,7 +14,7 @@ class ProductSeeder extends Seeder
                 'judul' => 'Hoodie Sweater "TYE DIE PINK"',
                 'sub_judul' => 'Hoodie',
                 'gambar' => 'iklan/S0Nx0lY9J5DSzroUQIhV2jxZ0T5DwwXoew6AHXML.png',
-                'product_id' => 101,
+                'product_id' => 12,
             ],
         ]);
     }
